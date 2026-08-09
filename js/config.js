5475619ceb20e7336d84fad6f136bfec612c4943
M4lcolmT <twhan06@gmail.com>
2026-08-09 15:59:34 +0800

    [v3.1] updated API url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzIrN0t5vMsmDq36ulNmaMw0E_kW39P3kZcBzIHyMzp9CyTytXDb1ajUhZJlIqUgHtepw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzegjEuuEAziL36r2ZDopLD_-sNPiOrJ5Igj6UuuWTyq5DcQW9Uu72ikxEwymOP9GnwlA/exec',
   API_KEY: 'zbjVTBrFJ96UvL5FRDwEoQEYqpqCLeWi',
   CACHE_MINUTES: 5,
   CURRENCY: 'MYR',
